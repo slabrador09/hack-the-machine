378e0eb4d8e86f04438169616b26ecd470b511aa
Sebastian Labrador <slabrador09@gmail.com>
2019-03-03 22:51:54 -0600

    adding ask sdk

--- htm-bot/app.js
+++ htm-bot/app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const Alexa = require('ask-sdk');
 
 app.get('/', (req, res) => res.send('HackTheMachine!'))
 app.get('/htmbot', (req, res) => res.send('HackTheMachine!'))
 app.post('/htmbot', (req, res) => {
-    res.status(201).json({msg: 'request accepted'})
+    res.status(201).json({ 
+        msg: 'request accepted'
+    })
 })
 
 app.listen(port, () => console.log(`HtmBot is listening on port ${port}!`))
